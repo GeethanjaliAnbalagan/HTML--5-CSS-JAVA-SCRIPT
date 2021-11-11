c1c0870e52763cc9781e9e4b08b5f20ff4ed8c2b
GeethanjaliAnbalagan <84552111+GeethanjaliAnbalagan@users.noreply.github.com>
2021-11-11 16:26:18 +0530

    Add files via upload

---
+++ JS FUNDAMENDALS/FUNCTIONAL EXPRESSION/hello.js
@@ -0,0 +1,13 @@
+ function myfunction(name){
+
+		let a = "hello,${name}";
+                  debugger;
+
+                  say(a);
+                   
+			}
+function say(a){
+
+alert('${a}');
+
+}
